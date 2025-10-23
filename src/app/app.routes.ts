3dc59ef3dd6c4173bd982eec2bef589cdc673962
Wilfredo Abregu <fabregu01@gmail.com>
2025-10-22 23:32:41 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'layout', pathMatch: 'full'},
+    {path: 'layout', component: LayoutComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: '**', redirectTo: 'layout', pathMatch: 'full'},
+];
---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [NavbarComponent, FooterComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
