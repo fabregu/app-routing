176dd20b85b35201ab03c3751679c29f9d73f637
Wilfredo Abregu <fabregu01@gmail.com>
2025-10-25 16:55:18 -0500

    refactor: elimiando imports que no se usan

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
-import { ListUsuariosComponent } from "./list-usuarios/list-usuarios.component";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, ListUsuariosComponent, RouterOutlet],
+  imports: [NavbarComponent, FooterComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
